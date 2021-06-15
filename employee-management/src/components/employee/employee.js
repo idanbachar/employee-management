c7daeb515361158abe0b19052248e9bd5b5ce80e
Idan Bachar <55582129+idanbachar@users.noreply.github.com>
2021-06-15 11:30:40 +0300

    added employees fields and react-bootstrap library

---
+++ employee-management/src/components/employee/employee.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Employee = (props) => {
+
+    return (
+        <div>
+ 
+        </div>
+    )
+
+}
+
+export default Employee;
--- employee-management/src/pages/employees.js
+++ employee-management/src/pages/employees.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Employee from '../components/employee/employee';
+import Table from 'react-bootstrap/Table';
 
-const employees = () => {
+const Employees = () => {
 
-    return (<h1>employees</h1>);
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(async () => {
+
+        const req = await fetch("http://localhost:3000/employees");
+        const data = await req.json();
+
+        setEmployees(data);
+
+    }, [])
+
+    return (
+        <div>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Roll</th>
+                        <th>Start Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(e =>
+                        <tr>
+                            <td>{e.firstname}</td>
+                            <td>{e.lastname}</td>
+                            <td>{e.phone}</td>
+                            <td>{e.address}</td>
+                            <td>{e.roll}</td>
+                            <td>{e.startdate}</td>
+                        </tr>)}
+
+                </tbody>
+            </Table>
+
+        </div>
+    )
 
 }
 
-export default employees;
+export default Employees;
