605974c6c78c23dbcb78a10a9fcdd054ced1e776
Idan Bachar <55582129+idanbachar@users.noreply.github.com>
2021-06-15 15:48:10 +0300

    Added option to add employee

---
+++ employee-management/src/components/AddEmployee/addEmployee.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+
+
+const AddEmployee = (props) => {
+
+    const [firstname, setFirstName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+
+    const validateData = () => {
+
+        const data = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "phone": phone,
+            "address": address,
+            "roll": "HR",
+            "startdate": "2 Feb 2020"
+        }
+        props.addEmployee(data);
+    }
+
+
+    return (
+        <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Add Employee
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid">
+                <Container>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formGroupFirstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter First Name" onChange={(e) => setFirstName(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formGroupLastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Last Name" onChange={(e) => setLastName(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formGroupPhone">
+                            <Form.Label>Phone</Form.Label>
+                            <Form.Control type="phone" placeholder="Enter Phone Number" onChange={(e) => setPhone(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formGroupAddress">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Address" onChange={(e) => setAddress(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formGroupRoll">
+                            <Form.Select >
+                                <option>Disabled select</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                    </Form>
+
+
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+                <Button onClick={() => validateData()}> Add Employee!</Button>
+            </Modal.Footer>
+        </Modal >
+    );
+}
+
+
+export default AddEmployee;
--- employee-management/src/pages/manage.js
+++ employee-management/src/pages/manage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import AddEmployee from '../components/AddEmployee/addEmployee';
+import { v4 as uuidv4 } from 'uuid';
 
 const api = axios.create({
     baseURL: `http://localhost:3000/employees`
@@ -9,55 +12,77 @@ const api = axios.create({
 const Manage = () => {
 
     const [employees, setEmployees] = useState([]);
+    const [addEmployeeModalShow, setEmployeeModalShow] = useState(false);
 
     useEffect(() => {
         getEmployees();
     }, [])
 
     const getEmployees = async () => {
-        let data = await api.get('/').then(({ data }) => data);
+        let data = await api.get('/')
+            .then(({ data }) => data);
         setEmployees(data);
     }
 
     const deleteEmployee = async (id) => {
-
         let data = await api.delete(`/${id}`);
         getEmployees();
     }
 
+    const addEmployee = async (employee) => {
+        const request = {
+            id: uuidv4(),
+            ...employee
+        };
+
+        const response = await api.post(`/`, request);
+        let updatedEmployees = [...employees];
+        updatedEmployees.push(response.data);
+        setEmployees(updatedEmployees);
+        setEmployeeModalShow(false);
+    }
+
     return (
-        <div style={{ backgroundColor: 'white', fontSize: '20px', textAlign: 'center' }}>
+        <div>
 
-            <Table>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Phone</th>
-                        <th>Address</th>
-                        <th>Roll</th>
-                        <th>Start Date</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(e =>
+            <Button variant="primary" onClick={() => setEmployeeModalShow(true)}>
+                Add Employee
+            </Button>
+
+            <AddEmployee addEmployee={addEmployee} show={addEmployeeModalShow} onHide={() => setEmployeeModalShow(false)} />
+
+            <div style={{ backgroundColor: 'white', fontSize: '20px', textAlign: 'center' }}>
+
+                <Table>
+                    <thead>
                         <tr>
-                            <td>{e.firstname}</td>
-                            <td>{e.lastname}</td>
-                            <td>{e.phone}</td>
-                            <td>{e.address}</td>
-                            <td>{e.roll}</td>
-                            <td>{e.startdate}</td>
-                            <td><button>Edit</button><button onClick={() => deleteEmployee(e.id)}>Remove</button></td>
-                        </tr>)}
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Roll</th>
+                            <th>Start Date</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map(e =>
+                            <tr>
+                                <td>{e.firstname}</td>
+                                <td>{e.lastname}</td>
+                                <td>{e.phone}</td>
+                                <td>{e.address}</td>
+                                <td>{e.roll}</td>
+                                <td>{e.startdate}</td>
+                                <td><button>Edit</button><button onClick={() => deleteEmployee(e.id)}>Remove</button></td>
+                            </tr>)}
 
-                </tbody>
-            </Table>
+                    </tbody>
+                </Table>
 
-        </div >
+            </div >
+        </div>
     )
-
 }
 
 export default Manage;
